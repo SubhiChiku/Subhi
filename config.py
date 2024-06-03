e8a1be8b331c48738c9c539781d4264c8be4554d
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-06-03 17:59:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = getenv("LOGGER_ID", -1002207550354))
+LOGGER_ID = getenv("LOGGER_ID", None)
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6134246701))
